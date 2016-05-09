2734d4b7c2fa2363cffba1a7220f9620105c12b3
himanshu <himanshu81494@gmail.com>
2016-05-09 14:04:00 +0530

    Update clarifai_client.js

--- js/clarifai_client.js
+++ js/clarifai_client.js
@@ -98,7 +98,7 @@ function parseResponse(resp) {
 }
 
 function run(imgurl, mode) {
-  if(localStorage.getItem('tokenTimeStamp') - Math.floor(Date.now() / 1000) > 86400
+  if(Math.floor(Date.now() / 1000) - localStorage.getItem('tokenTimeStamp') > 86400
     || localStorage.getItem('accessToken') === null) {
     getCredentials(function() {
       postImageNSFW(imgurl, mode);
